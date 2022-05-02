7830d4f3f980c6b4b061db68f2910ab03b5aaa01
Anna Tabeeva <anyuta_1984@mail.ru>
2022-05-02 15:22:23 +0400

    Убрала параметризацию в тесте test_guest_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -29,8 +29,9 @@ class TestUserAddToBasketFromProductPage():
         page.check_price()
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('num', [*range(0,7), pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
-def test_guest_can_add_product_to_basket(browser, num):
+#@pytest.mark.parametrize('num', [*range(0,7), pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
+def test_guest_can_add_product_to_basket(browser): #(browser, num)
+    num = 1
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}'    
     page = ProductPage(browser, link)    
     page.open()    
